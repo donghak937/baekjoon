58c16618cf545c449b1a716fae5f26aadc498a1e
donghak937 <donghak937@gmail.com>
2025-03-16 00:10:45 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for (int i = 0; i < myString.length(); i++){
+        if(myString[i] == 'a') myString[i] -= 32;
+        
+        else if ( 'B' <= myString[i] &&  myString[i] <= 'Z') myString[i] += 32;
+    }
+    return myString;
+}
