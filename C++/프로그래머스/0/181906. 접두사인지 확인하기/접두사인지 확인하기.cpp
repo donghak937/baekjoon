28cea366541a58c4e3de93de9c3d1c2b999ab40b
donghak937 <donghak937@gmail.com>
2025-03-24 08:38:28 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 1;
+    for(int i = 0; i < is_prefix.length(); i++){
+        if (is_prefix[i] != my_string[i]) return 0;
+    }
+    return answer;
+}
