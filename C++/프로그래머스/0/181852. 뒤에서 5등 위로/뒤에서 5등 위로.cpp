08b9d64050602ec32b5f4f3ce1f795faababff5e
donghak937 <donghak937@gmail.com>
2025-03-25 10:59:00 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    int temp = num_list[0];
+    vector<int> answer;
+    for(int i = 0; i < num_list.size(); i++){
+        for(int j = 0; j < num_list.size(); j++){
+            if (num_list[i] < num_list[j]){
+                temp = num_list[i];
+                num_list[i] = num_list[j];
+                num_list[j] = temp;
+            }
+        }
+    }
+    
+    for(int i = 5; i < num_list.size() ; i++){
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
