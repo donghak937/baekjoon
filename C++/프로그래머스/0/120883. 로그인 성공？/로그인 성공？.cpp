420a0eca66de26fe4c5353957f3f1c740fc01fed
dongha <donghak937@gmail.com>
2025-04-04 18:09:39 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "";
+    for(int i = 0; i < db.size(); i++){
+        if (id_pw[0] == db[i][0]){
+            if (id_pw[1] == db[i][1]) {
+                return "login";
+            }
+            else return "wrong pw";
+        }
+    }
+    return "fail";
+}
