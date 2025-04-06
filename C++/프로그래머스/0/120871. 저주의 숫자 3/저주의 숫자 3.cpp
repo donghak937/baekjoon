cbb32208159263d177c2af84a656a10b81c4d82b
dongha <donghak937@gmail.com>
2025-04-06 10:13:37 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+
+int solution(int n) {
+    int i = 1;
+    int a = 0;
+    while (1) {
+        if (i == n+1) break;
+        a++;
+        while (1){
+            if (a % 10 != 3 && a % 3 != 0) break;
+            if (a % 10 == 3) a++;
+            if (a % 3 == 0) a++;
+            if ((a / 10 >= 3 && a / 10 < 4)) {
+                while (!(a / 10 >= 4)) {
+                    a++;
+                }
+            }
+            if ((a / 10 >= 13 && a / 10 < 14)) {
+                while (!(a / 10 >= 14)) {
+                    a++;
+                }
+            }             
+        }
+        i++;
+        
+    }
+    return a;
+}
+
