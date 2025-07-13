3df53103298336704653ba0640a0d55703e1df26
dongha <donghak937@gmail.com>
2025-07-14 03:02:48 +0900

    [level 2] Title: H-Index, Time: 1.39 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int hcount = 0;
+    int count = 0;
+    int max = 0;
+    sort(citations.begin(), citations.end());
+    for(int i = 0; i <= citations[citations.size() - 1]; i++){
+        for(int j = 0; j < citations.size(); j++){
+            if (i <= citations[j]) {
+                hcount++;
+            }
+            else count++;
+        }
+        
+        if (i <= hcount && i > count){
+            max = i;
+        }
+        hcount = 0;
+        count = 0;
+    }
+    return max;
+}
