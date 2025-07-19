22de91c8e21d20b828226908e88bb1a8d030dbce
dongha <donghak937@gmail.com>
2025-07-19 15:18:44 +0900

    [Bronze II] Title: 벨트, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/10834. 벨트/벨트.cc
+++ C++17/백준/Bronze/10834. 벨트/벨트.cc
@@ -1,28 +1,23 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
-
 using namespace std;
-
-int main() {
-    int N;
-    cin >> N;
-    int ans1 = 0, ans2 = 0;
-    int tmp1, tmp2, tmp3;
-    int rot = 0;
-    for(int i = 0; i < N; i++){
-        cin >> tmp1 >> tmp2 >> tmp3;
-        if (i == 0) {
-            ans1 = tmp1;
-            ans2 = tmp2;
-        }
-        else{
-            ans1 *= tmp1;
-            ans2 *= tmp2;
-        }
-
-        if (tmp3 == 1) rot = rot == 1 ? 0 : 1;
-    }
-
-    cout << rot << " " << (ans2 / ans1);
+int main()
+{
+	// M : 벨트의 개수
+	// a,b :벨트로 이어진 두 바퀴의 회전수의 비
+	// s : 벨트의 형태 (안꼬인 형태는 0,꼬인 형태는 1)
+	// r : 회전 방향 (시계방향은 true, 반시계방향은 false), 첫 시작은 시계방향 
+	// r_num : M+1번 바퀴의 분당 회전수
+	int M, a=1, b=1,s,r_num=1; 
+	bool r = true;
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> a >> b >> s;
+		r_num = (r_num / a)*b;
+		r = s == 1 ? !r : r; // 꼬인 형태면 방향이 반대
+	}
+	
+	if (r) cout << "0 " << r_num;
+	else cout <<"1 " << r_num;
+	return 0;
 }
