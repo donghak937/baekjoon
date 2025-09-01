7b3ed11cbfc25913a99945a6ffbb7b2ad2e93f87
dongha <donghak937@gmail.com>
2025-09-01 16:13:00 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    long long a;
+    cin >> a;
+    cout << (a * (a - 1)) / 2 << '\n' << "2";
+}
