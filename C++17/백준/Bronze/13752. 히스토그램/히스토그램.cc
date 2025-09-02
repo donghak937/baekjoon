2dd1fc39f9a9118b8f74a76d9cfdd6997c1816b4
dongha <donghak937@gmail.com>
2025-09-02 14:53:18 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+
+        for(int j = 0; j < a; j++){
+            cout << "=";
+        }
+        cout << endl;
+    }
+}
