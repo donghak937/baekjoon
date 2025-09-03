05f1b97e0f543b70f56e830d35df67b88851bc44
dongha <donghak937@gmail.com>
2025-09-03 12:23:45 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/15829. Hashing/Hashing.cc
+++ C++17/백준/Bronze/15829. Hashing/Hashing.cc
@@ -10,8 +10,10 @@ int main() {
     long long total = 0;
     long long r = 1;
     for(int i = 0; i < n; i++){
-        total += ((str[i] - 'a' + 1) * r) % 1234567891;
-        r *= 31;
+        long long v (str[i] - 'a' + 1);
+        total = (total + (v * r) % 1234567891) % 1234567891;
+        
+        r = (r * 31) % 1234567891;
 
     }
     cout << total;
