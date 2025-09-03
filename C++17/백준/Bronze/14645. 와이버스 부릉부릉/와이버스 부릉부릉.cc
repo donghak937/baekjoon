f6a1cabc6059430e3f852df4bf88589f08d75738
dongha <donghak937@gmail.com>
2025-09-03 20:49:58 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
+++ C++17/백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -3,15 +3,5 @@
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            int a;
-            cin >> a;
-        }
-    }
-
     cout << "비와이";
-
 }
