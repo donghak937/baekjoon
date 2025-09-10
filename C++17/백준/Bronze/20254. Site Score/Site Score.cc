8fb63a1ad10fa4a86c5aaff1cd597d3dd47b3e60
dongha <donghak937@gmail.com>
2025-09-10 22:41:31 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    long long a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    long long total = (a * 56) + (b * 24) + (c * 14) + (d * 6);
+
+    cout << total;
+}
