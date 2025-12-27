63b4243e26cd8d637bcfd645386da1a315c7a690
dongha <donghak937@gmail.com>
2025-12-27 20:21:53 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n-1; j++) {
+            cout << " ";
+        }
+        for(int j = 0; j < i*2+1; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
