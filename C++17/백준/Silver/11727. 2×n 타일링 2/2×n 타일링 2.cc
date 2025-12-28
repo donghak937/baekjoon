95802e40ad86645b3807799ba19549d6f67dbce3
dongha <donghak937@gmail.com>
2025-12-29 00:22:18 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int dp[10001];
+
+    cin >> N;
+
+    dp[1] = 1;
+    dp[2] = 3;
+    for(int i = 3; i <= N; i ++){
+        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+    }
+    cout << dp[N];
+}
