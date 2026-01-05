1f6cb70be7621f13cef419125158891ec4d40c13
dongha <donghak937@gmail.com>
2026-01-05 18:27:58 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 3020 ms, Memory: 16152 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int n,m;
+
+    cin >> n >> m;
+
+    map<string, string> maps;
+
+    for(int i = 0; i < n; i++){
+        string a, b;
+        cin >> a >> b;
+        maps.insert({a,b});
+    }
+
+    for(int i = 0; i < m; i++){
+        string a;
+        cin >> a;
+        cout << maps[a] << '\n';
+    }
+
+
+
+}
