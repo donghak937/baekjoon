205462475c85b2d3501fa2a487ed102edfd0daf9
dongha <donghak937@gmail.com>
2026-01-28 11:08:34 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+        int x1 = 99, y1 = 99, x2 = 0, y2 = 0;
+
+    for(int i = 0; i < wallpaper.size(); i++){
+        for(int j = 0; j < wallpaper[0].size(); j++){
+            if (wallpaper[i][j] == '#'){
+                if(i < x1) x1 = i; 
+                if(i > x2) x2 = i; 
+                if(j < y1) y1 = j; 
+                if(j > y2) y2 = j; 
+            }
+
+        }
+    }
+
+    answer.push_back(x1);
+    answer.push_back(y1);
+    answer.push_back(x2+1);
+    answer.push_back(y2+1);
+    return answer;
+}
